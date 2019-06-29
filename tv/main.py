e04230765fad5da847894408d2056f6839d43217
Jean-Frederic Mainville <jfmainville@outlook.com>
2019-06-29 17:06:02 -0400

    Add the tmdb prefix to the show variables
    - Rename the extract_watchlist_series function to tmdb_extract_watchlist_series
    - Rename the extract_show_details function to tmdb_extract_show_details
    - Update the extract_watchlist_series and extract_show_details functions import
    - Add the tmdb prefix to all the show variables

--- tv/main.py
+++ tv/main.py
@@ -1,7 +1,7 @@
 import os
 import json
 from authenticate import authenticate
-from watchlist import extract_watchlist_series, extract_show_details
+from watchlist import tmdb_extract_watchlist_series, tmdb_extract_show_details
 from folder import create_tv_show_folder, get_tv_show_folder_episodes
 from eztv import eztv_extract_tv_show_episodes
 
@@ -21,20 +21,22 @@ tmdb_session_id = authenticate(tmdb_api_url=tmdb_api_url, tmdb_username=tmdb_use
                                tmdb_password=tmdb_password, tmdb_api_key=tmdb_api_key, tmdb_account_id=tmdb_account_id)
 
 # Extract the shows details from the TMDB API
-shows = extract_watchlist_series(
+tmdb_shows = tmdb_extract_watchlist_series(
     tmdb_api_url=tmdb_api_url, tmdb_account_id=tmdb_account_id, tmdb_session_id=tmdb_session_id, tmdb_api_key=tmdb_api_key)
 
-for show in shows:
-    show_details = extract_show_details(tmdb_api_url=tmdb_api_url,
-                                        tmdb_api_key=tmdb_api_key, show=show)
-    show_name = show["name"]
-    show_season = str(
-        show_details["last_episode_to_air"]["season_number"]).zfill(2)
+for tmdb_show in tmdb_shows:
+    tmdb_show_details = tmdb_extract_show_details(tmdb_api_url=tmdb_api_url,
+                                                  tmdb_api_key=tmdb_api_key, tmdb_show=tmdb_show)
+    tmdb_show_id = (
+        tmdb_show_details["external_ids"]["imdb_id"]).replace("tt", "")
+    tmdb_show_name = tmdb_show["name"]
+    tmdb_show_season = str(
+        tmdb_show_details["last_episode_to_air"]["season_number"]).zfill(2)
     # Create the local TV show directory if it doesn't already exists
     create_tv_show_folder(
-        tv_shows_directory=tv_shows_directory, show_name=show_name)
+        tv_shows_directory=tv_shows_directory, show_name=tmdb_show_name)
     # List all the TV show episodes that were already downloaded
     tv_show_directory_episodes = get_tv_show_folder_episodes(
-        tv_shows_directory=tv_shows_directory, show_name=show_name)
+        tv_shows_directory=tv_shows_directory, show_name=tmdb_show_name)
     # Extract the list of the TV show episodes available on EZTV
     eztv_shows = eztv_extract_tv_show_episodes(tmdb_show_id=tmdb_show_id)
--- tv/watchlist.py
+++ tv/watchlist.py
@@ -4,7 +4,7 @@ import os
 
 
 # Send a GET request to get the list of series in the watchlist
-def extract_watchlist_series(tmdb_api_url, tmdb_account_id, tmdb_session_id, tmdb_api_key):
+def tmdb_extract_watchlist_series(tmdb_api_url, tmdb_account_id, tmdb_session_id, tmdb_api_key):
     api_connection = http.client.HTTPSConnection(tmdb_api_url)
     api_connection.request("GET", "/3/account/" + tmdb_account_id + "/watchlist/tv?api_key=" +
                            tmdb_api_key + "&language=en-US&session_id=" + tmdb_session_id + "&sort_by=created_at.asc")
@@ -14,10 +14,10 @@ def extract_watchlist_series(tmdb_api_url, tmdb_account_id, tmdb_session_id, tmd
     return watchlist_series["results"]
 
 
-def extract_show_details(tmdb_api_url, tmdb_api_key, show):
+def tmdb_extract_show_details(tmdb_api_url, tmdb_api_key, tmdb_show):
     api_connection = http.client.HTTPSConnection(tmdb_api_url)
     api_connection.request("GET",
-                           "/3/tv/" + str(show["id"]) + "?api_key=" + tmdb_api_key + "&language=en-US")
+                           "/3/tv/" + str(tmdb_show["id"]) + "?api_key=" + tmdb_api_key + "&language=en-US&append_to_response=external_ids")
     show_details_response = api_connection.getresponse()
     show_details_data = show_details_response.read()
     show_details = json.loads(show_details_data)
