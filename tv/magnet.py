09c3ca70df3375e5c3db0ab5b48c7d331eb8e856
Jean-Frederic Mainville <jfmainville@outlook.com>
2019-08-09 16:40:15 -0400

    Add the timeout property to the subprocess command in the download_magnet_link function

--- tv/magnet.py
+++ tv/magnet.py
@@ -8,7 +8,7 @@ def download_magnet_link(download_tv_show, tv_shows_download_directory):
         # Download only torrents that have 5 seeds or more
         if download_tv_show["seeds"] >= 5:
             subprocess.check_output(
-                ["aria2c", "-d", tv_shows_download_directory, "--bt-stop-timeout=300", "--seed-time=0", download_tv_show["magnet"]])
+                ["aria2c", "-d", tv_shows_download_directory, "--bt-stop-timeout=300", "--seed-time=0", download_tv_show["magnet"]], timeout=600)
             returncode = 0
             return returncode
         else:
