ad525fcdb5f5237f396a083d59782452a4816fd9
Jean-Frederic Mainville <jfmainville@outlook.com>
2019-08-08 20:58:41 -0400

    Update the download_magnet_link function

--- tv/magnet.py
+++ tv/magnet.py
@@ -5,10 +5,14 @@ import json
 def download_magnet_link(download_tv_show, tv_shows_download_directory):
     # Download the TV show using the magnet link
     try:
-        subprocess.check_output(
-            ["aria2c", "-d", tv_shows_download_directory, "--bt-stop-timeout=3", "--seed-time=0", download_tv_show["magnet"]])
-        returncode = 0
-        return returncode
+        if download_tv_show["seeds"] >= 5:
+            subprocess.check_output(
+                ["aria2c", "-d", tv_shows_download_directory, "--bt-stop-timeout=900", "--seed-time=0", download_tv_show["magnet"]])
+            returncode = 0
+            return returncode
+        else:
+            returncode = 7
+            return returncode
     except subprocess.CalledProcessError as error:
         # If a timeout occurs, create an empty file to prevent future downloads
         if error.returncode == 7:
