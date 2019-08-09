af1741fd4a92a20b03a81c2a18c5b5cba0507034
Jean-Frederic Mainville <jfmainville@outlook.com>
2019-08-08 20:59:02 -0400

    Remove the test data for the download_magnet_link function

--- tv/magnet.py
+++ tv/magnet.py
@@ -18,9 +18,3 @@ def download_magnet_link(download_tv_show, tv_shows_download_directory):
         if error.returncode == 7:
             returncode = 7
             return returncode
-
-
-download_tv_show = {'name': 'Last Week Tonight With John Oliver S06E14', 'seeds': 30,
-                    'magnet': 'magnet:?xt=urn:btih:06fe9d2b2e08ac20ebb61a0d896cc842bbf2f949&dn=Last.Week.Tonight.With.John.Oliver.S06E14.HDTV.x264-UAV%5Beztv%5D&tr=udp://tracker.coppersurfer.tk:80&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://exodus.desync.com:6969'}
-download_magnet_link(download_tv_show=download_tv_show,
-                     tv_shows_download_directory="/app/watch/downloads/tv")
