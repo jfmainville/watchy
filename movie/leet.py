6d2262e5b946eaa3a389036e756f906214e30ee0
Jean-Frederic Mainville <jfmainville@outlook.com>
2019-08-11 15:22:19 -0400

    Add the leet_extract_movie function to the leet.py file

--- movie/leet.py
+++ movie/leet.py
@@ -1,8 +1,44 @@
-import http
-import json
-import ssl
+import time
+from selenium import webdriver
 
 
-def leet_extract_movies(tmdb_movie_id):
-    # Extract the movies list from the leet site
-    pass
+def leet_extract_movies(movie_title):
+    # Extract the movies list from the 1337x site
+    chrome_path = r'/usr/lib/chromium/chromedriver'
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1200x600')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    chrome = webdriver.Chrome(
+        executable_path=chrome_path, chrome_options=options)
+
+    # Navigate to the 1337x site and execute a search sorted search based on the number of seeds
+    leet_url = "https://1337x.to"
+    encoded_movie_title = movie_title.replace(" ", "%20")
+    chrome.get(leet_url + "/sort-search/" +
+               encoded_movie_title + "/seeders/desc/1/")
+    time.sleep(5)
+
+    leet_movie_torrents = chrome.find_elements_by_class_name("name")
+    # 1337x torrent movies list
+    leet_movies_list = []
+    # Create a list of all the torrent movies
+    for leet_movie_torrent in leet_movie_torrents:
+        for leet_movie_torrent_name in leet_movie_torrent.get_attribute("innerText").split("\n"):
+            if movie_title in leet_movie_torrent_name:
+                leet_movies_list.append(leet_movie_torrent_name)
+    # Extract the full URL of the torrent with the most seeds
+    torrent_link = chrome.find_element_by_partial_link_text(
+        leet_movies_list[0]).get_attribute("href")
+    # Navigate to the torrent page
+    chrome.get(torrent_link)
+    time.sleep(5)
+    # Extract the number of seeds from the torrent page
+    seeds = chrome.find_elements_by_class_name(
+        "seeds")[0].get_attribute("innerText")
+    # Extract the magnet link from the torrent page
+    magnet_link = chrome.find_element_by_css_selector(
+        '[href^=magnet]').get_attribute("href")
+    chrome.quit()
+    return seeds, magnet_link
