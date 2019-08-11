7905c3c0b11dce0d2233b29d019faaa78de0c2a1
Jean-Frederic Mainville <jfmainville@outlook.com>
2019-08-11 16:50:25 -0400

    Add a condition to return an empty dict if the movie release date has not been set

--- movie/main.py
+++ movie/main.py
@@ -34,21 +34,26 @@ for tmdb_watchlist_movie in tmdb_watchlist_movies:
     tmdb_movie_title = tmdb_watchlist_movie["title"]
     tmdb_movie_release_year = (
         tmdb_watchlist_movie["release_date"]).split("-")[0]
+    tmdb_movie_dvd_release_date = []
     for tmdb_movie_release_date in tmdb_movie_release_dates["results"]:
         for release_dates in tmdb_movie_release_date["release_dates"]:
-            # Check that the rel
+            # Check that the release date is set
             if release_dates["type"] == 4 and tmdb_movie_release_date["iso_3166_1"] == "BR":
-                tmdb_movie_dvd_release_date = (
-                    release_dates["release_date"]).split("T")[0]
+                tmdb_movie_dvd_release_date.append(
+                    release_dates["release_date"].split("T")[0])
     # Create the local movies directory if it doesn't already exists
     create_movie_folders(
         movies_directory=movies_directory, movies_download_directory=movies_download_directory, movie_title=tmdb_movie_title)
     # List all the movies that were already downloaded
     local_movies = get_local_movies(
         movies_directory=movies_directory, movie_title=tmdb_movie_title)
-    # Convert the DVD release date to a time format
-    tmdb_movie_dvd_release_date_convert = time.strptime(
-        str(tmdb_movie_dvd_release_date), "%Y-%m-%d")
+    if tmdb_movie_dvd_release_date != []:
+        # Convert the DVD release date to a time format
+        tmdb_movie_dvd_release_date_convert = time.strptime(
+            str(tmdb_movie_dvd_release_date[0]), "%Y-%m-%d")
+    else:
+        tmdb_movie_dvd_release_date_convert = time.strptime(
+            str(date.today()), "%Y-%m-%d")
     today = time.strptime(str(date.today()), "%Y-%m-%d")
     # Check if the DVD release date is earlier than today
     if tmdb_movie_dvd_release_date_convert < today:
@@ -66,9 +71,10 @@ for tmdb_watchlist_movie in tmdb_watchlist_movies:
                 "magnet": magnet_link
             })
             break
-        # Download the movie magnet using the aria2 application
-        returncode = download_magnet_link(download_movie=download_movie,
-                                          movies_download_directory=movies_download_directory)
-        # Move the movie download file to the movies directory
-        move_local_movie(download_movie=download_movie, movies_download_directory=movies_download_directory,
-                         movies_directory=movies_directory, movie_title=tmdb_movie_title, returncode=returncode)
+        if download_movie != {}:
+            # Download the movie magnet using the aria2 application
+            returncode = download_magnet_link(download_movie=download_movie,
+                                              movies_download_directory=movies_download_directory)
+            # Move the movie download file to the movies directory
+            move_local_movie(download_movie=download_movie, movies_download_directory=movies_download_directory,
+                             movies_directory=movies_directory, movie_title=tmdb_movie_title, returncode=returncode)
