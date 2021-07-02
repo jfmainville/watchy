6397913cbecc7aa2c67f987da822fd1f4704ca87
Jean-Frederic Mainville <jfmainville@outlook.com>
2021-07-02 17:49:39 -0400

    Add logging configuration to the tmdb_authenticate function
    Add the logging configuration to the tmdb_authenticate function.

--- watchy/tmdb.py
+++ watchy/tmdb.py
@@ -9,33 +9,63 @@ def tmdb_authenticate(tmdb_api_url, tmdb_username, tmdb_password, tmdb_api_key):
     headers = {
         "Content-Type": "application/json"
     }
-    request_token_data = requests.get(tmdb_api_url +
-                                      "/3/authentication/token/new?api_key=" + tmdb_api_key, headers=headers)
-    request_token = request_token_data.json()['request_token']
+    request_token = None
+    try:
+        request_token_data = requests.get(tmdb_api_url +
+                                          "/3/authentication/token/new?api_key=" + tmdb_api_key, headers=headers)
+        request_token = request_token_data.json()['request_token']
+    except requests.exceptions.ConnectionError as error:
+        logger.error("requests connection error: %s", error)
+    except requests.exceptions.Timeout as error:
+        logger.error("requests connection timeout: %s", error)
+    except requests.exceptions.HTTPError as error:
+        logger.error("requests HTTP error: %s", error)
 
     # Send a POST request to validate the request token with the username and password
-    validate_login_payload = {
-        "username": tmdb_username,
-        "password": tmdb_password,
-        "request_token": request_token
-    }
-    tmdb_validate_login_response = requests.post(
-        tmdb_api_url + "/3/authentication/token/validate_with_login?api_key=" + tmdb_api_key,
-        params=validate_login_payload, headers=headers)
-
-    tmdb_session_id = None
-
-    if tmdb_validate_login_response.status_code == 200:
-        # Send a POST request to get the session ID
-        request_token_payload = {
+    if request_token:
+        validate_login_payload = {
+            "username": tmdb_username,
+            "password": tmdb_password,
             "request_token": request_token
         }
-        tmdb_session_id = requests.post(tmdb_api_url +
-                                        "/3/authentication/session/new?api_key=" + tmdb_api_key,
-                                        params=request_token_payload,
-                                        headers=headers)
-        tmdb_session_id = tmdb_session_id.json()['session_id']
-    return tmdb_session_id
+        tmdb_validate_login_response = None
+        try:
+            tmdb_validate_login_response = requests.post(
+                tmdb_api_url + "/3/authentication/token/validate_with_login?api_key=" + tmdb_api_key,
+                params=validate_login_payload, headers=headers)
+        except requests.exceptions.ConnectionError as error:
+            logger.error("requests connection error: %s", error)
+        except requests.exceptions.Timeout as error:
+            logger.error("requests connection timeout: %s", error)
+        except requests.exceptions.HTTPError as error:
+            logger.error("requests HTTP error: %s", error)
+
+        tmdb_session_id = None
+
+        if tmdb_validate_login_response and tmdb_validate_login_response.status_code == 200:
+            # Send a POST request to get the session ID
+            request_token_payload = {
+                "request_token": request_token
+            }
+
+            try:
+                tmdb_session_id = requests.post(tmdb_api_url +
+                                                "/3/authentication/session/new?api_key=" + tmdb_api_key,
+                                                params=request_token_payload,
+                                                headers=headers)
+                tmdb_session_id = tmdb_session_id.json()['session_id']
+            except requests.exceptions.ConnectionError as error:
+                logger.error("requests connection error: %s", error)
+            except requests.exceptions.Timeout as error:
+                logger.error("requests connection timeout: %s", error)
+            except requests.exceptions.HTTPError as error:
+                logger.error("requests HTTP error: %s", error)
+
+    if request_token and tmdb_session_id:
+        return tmdb_session_id
+    else:
+        logger.error("failed to authenticate the user")
+        return request_token
 
 
 def tmdb_extract_watchlist(tmdb_api_url, tmdb_account_id, tmdb_session_id, tmdb_api_key, tmdb_watchlist_content_type):
