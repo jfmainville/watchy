e68eb954aa204ba56d5ab89c54fbca033ed854e8
Jean-Frederic Mainville <jfmainville@outlook.com>
2021-07-02 17:27:13 -0400

    Add logging configuration to the download_magnet_link function
    Add the logging configuration to the download_magnet_link function. This new configuration will now provide more information regarding the status of the torrent download process.

--- watchy/magnet.py
+++ watchy/magnet.py
@@ -1,4 +1,7 @@
 import subprocess
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def download_magnet_link(download_entry, download_directory):
@@ -6,21 +9,29 @@ def download_magnet_link(download_entry, download_directory):
     try:
         # Download only torrents that have 5 seeds or more
         if download_entry["seeds"] >= 5:
+            logger.info("starting download for %s", download_entry["title"])
             subprocess.check_output(
                 ["aria2c", "-d", download_directory, "--bt-stop-timeout=3500", "--seed-time=0",
                  download_entry["magnet"]], timeout=3600)
             return_code = 0
+            logger.info("completed download for {0} (return_code={1})".format(download_entry["title"], return_code))
             return return_code
         else:
             # Return code 7 if the amount of seeds is below 5
             return_code = 7
+            logger.info("less than 5 seeds for {0} (return_code={1})".format(download_entry["title"], return_code))
             return return_code
     except subprocess.CalledProcessError as error:
         # Return code 7 if the aria2c command line application crashes
         if error.returncode == 7:
             return_code = 7
+            logger.error(
+                "aria2c command crashed during execution for {0} (return_code={1})".format(download_entry["title"],
+                                                                                           return_code))
             return return_code
     except subprocess.TimeoutExpired as error:
         # Return code 2 if the aria2c command timeout
         return_code = 2
+        logger.error("aria2c command timeout during execution for {0} (return_code={1})".format(download_entry["title"],
+                                                                                                return_code))
         return return_code
