6ed58ffe397f376106c3262acd5ed672e1a602ca
Jean-Frederic Mainville <jfmainville@outlook.com>
2021-07-02 17:30:24 -0400

    Refactor error handling definition for the tv_show function
    Refactor the error handling definition for the tv_show function.

--- watchy/main.py
+++ watchy/main.py
@@ -155,62 +155,67 @@ def tv_show():
         tmdb_api_url=tmdb_api_url, tmdb_account_id=tmdb_account_id, tmdb_session_id=tmdb_session_id,
         tmdb_api_key=tmdb_api_key, tmdb_watchlist_content_type="tv")
 
-    for tmdb_show in tmdb_shows:
-        tmdb_show_details = tmdb_extract_show_details(tmdb_api_url=tmdb_api_url,
-                                                      tmdb_api_key=tmdb_api_key, tmdb_show=tmdb_show)
-        tmdb_show_id = (
-            tmdb_show_details["external_ids"]["imdb_id"]).replace("tt", "")
-        tmdb_show_name = re.sub('[^a-zA-Z0-9 \n]', '', unidecode.unidecode(tmdb_show["name"]))
-        # Create the local TV show directory if it doesn't already exists
-        create_content_folders(
-            content_folder=tv_shows_directory, content_download_folder=tv_shows_download_directory,
-            content_title=tmdb_show_name)
-        # List all the TV show episodes that were already downloaded
-        tv_show_directory_episodes = get_folder_content(
-            content_folder=tv_shows_directory, content_title=tmdb_show_name)
-        # Extract the list of the TV show episodes available on EZTV
-        eztv_shows = eztv_extract_tv_show_episodes(tmdb_show_id=tmdb_show_id, eztv_url=eztv_url)
-        eztv_show_listdict = []
-        for eztv_show in eztv_shows:
-            eztv_show_episode = "S" + str(eztv_show["season"]).zfill(2) + "E" + str(eztv_show["episode"]).zfill(2)
-            eztv_show_title = (eztv_show["title"]).split(eztv_show_episode)[0]
-            eztv_show_timestamp = eztv_show["date_released_unix"]
-            eztv_show_seeds = eztv_show["seeds"]
-            eztv_show_magnet = eztv_show["magnet_url"]
+    if tmdb_shows:
+        for tmdb_show in tmdb_shows:
+            tmdb_show_details = tmdb_extract_show_details(tmdb_api_url=tmdb_api_url,
+                                                          tmdb_api_key=tmdb_api_key, tmdb_show=tmdb_show)
 
-            # EZTV dictionary creation
-            eztv_show_full_name = tmdb_show_name.title() + " " + eztv_show_episode
-            if tmdb_show_name.lower() in eztv_show_title.lower():
-                eztv_show_listdict.append({
-                    "name": eztv_show_full_name,
-                    "seeds": eztv_show_seeds,
-                    "magnet": eztv_show_magnet,
-                    "timestamp": eztv_show_timestamp
-                })
-        # Refactor the eztv_show_listdict list dictionary to only show unique values
-        filtered_eztv_show_listdict = list(
-            {value['name']: value for value in eztv_show_listdict}.values())
-        # TV show  list that contains all the files that needs to be downloaded
-        download_tv_shows = []
-        for filtered_eztv_show_dictionary_item in filtered_eztv_show_listdict:
-            eztv_show_title_filtered = filtered_eztv_show_dictionary_item["name"]
-            while eztv_show_title_filtered not in tv_show_directory_episodes:
-                # Add the TV show episodes that needs to be downloaded to the list
-                download_tv_shows.append({
-                    "title": filtered_eztv_show_dictionary_item["name"],
-                    "seeds": filtered_eztv_show_dictionary_item["seeds"],
-                    "magnet": filtered_eztv_show_dictionary_item["magnet"]
-                })
-                break
-        for download_tv_show in download_tv_shows:
-            # Download the TV show magnet using the aria2 application
-            return_code = download_magnet_link(download_entry=download_tv_show,
-                                               download_directory=tv_shows_download_directory)
-            # Move the TV show download file to the TV shows directory
-            move_content_file(download_file=download_tv_show,
-                              content_download_folder=tv_shows_download_directory,
-                              content_folder=tv_shows_directory, content_title=tmdb_show_name,
-                              return_code=return_code)
+            if tmdb_show_details:
+                tmdb_show_id = (
+                    tmdb_show_details["external_ids"]["imdb_id"]).replace("tt", "")
+                tmdb_show_name = re.sub('[^a-zA-Z0-9 \n]', '', unidecode.unidecode(tmdb_show["name"]))
+                # Create the local TV show directory if it doesn't already exists
+                create_content_folders(
+                    content_folder=tv_shows_directory, content_download_folder=tv_shows_download_directory,
+                    content_title=tmdb_show_name)
+                # List all the TV show episodes that were already downloaded
+                tv_show_directory_episodes = get_folder_content(
+                    content_folder=tv_shows_directory, content_title=tmdb_show_name)
+                # Extract the list of the TV show episodes available on EZTV
+                eztv_shows = eztv_extract_tv_show_episodes(tmdb_show_id=tmdb_show_id, eztv_url=eztv_url)
+                eztv_show_listdict = []
+                for eztv_show in eztv_shows:
+                    eztv_show_episode = "S" + str(eztv_show["season"]).zfill(2) + "E" + str(eztv_show["episode"]).zfill(
+                        2)
+                    eztv_show_title = (eztv_show["title"]).split(eztv_show_episode)[0]
+                    eztv_show_timestamp = eztv_show["date_released_unix"]
+                    eztv_show_seeds = eztv_show["seeds"]
+                    eztv_show_magnet = eztv_show["magnet_url"]
+
+                    # EZTV dictionary creation
+                    eztv_show_full_name = tmdb_show_name.title() + " " + eztv_show_episode
+                    if tmdb_show_name.lower() in eztv_show_title.lower():
+                        eztv_show_listdict.append({
+                            "name": eztv_show_full_name,
+                            "seeds": eztv_show_seeds,
+                            "magnet": eztv_show_magnet,
+                            "timestamp": eztv_show_timestamp
+                        })
+                # Refactor the eztv_show_listdict list dictionary to only show unique values
+                filtered_eztv_show_listdict = list(
+                    {value['name']: value for value in eztv_show_listdict}.values())
+                # TV show  list that contains all the files that needs to be downloaded
+                download_tv_shows = []
+                for filtered_eztv_show_dictionary_item in filtered_eztv_show_listdict:
+                    eztv_show_title_filtered = filtered_eztv_show_dictionary_item["name"]
+                    while eztv_show_title_filtered not in tv_show_directory_episodes:
+                        # Add the TV show episodes that needs to be downloaded to the list
+                        download_tv_shows.append({
+                            "title": filtered_eztv_show_dictionary_item["name"],
+                            "seeds": filtered_eztv_show_dictionary_item["seeds"],
+                            "magnet": filtered_eztv_show_dictionary_item["magnet"]
+                        })
+                        break
+                for download_tv_show in download_tv_shows:
+                    # Download the TV show magnet using the aria2 application
+                    return_code = download_magnet_link(download_entry=download_tv_show,
+                                                       download_directory=tv_shows_download_directory)
+                    # Move the TV show download file to the TV shows directory
+                    move_content_file(download_file=download_tv_show,
+                                      content_download_folder=tv_shows_download_directory,
+                                      content_folder=tv_shows_directory, content_title=tmdb_show_name,
+                                      return_code=return_code)
+                    logger.info("tv_show")
 
 
 if args.movie:
