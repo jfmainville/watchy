d5207e940db3be365d7c3da726084b43dfefec40
Jean-Frederic Mainville <jfmainville@outlook.com>
2021-07-02 17:25:59 -0400

    Add logging configuration to the eztv_extract_tv_show_episodes function
    Add the logging configuration to the eztv_extract_tv_show_episodes function.

--- watchy/eztv.py
+++ watchy/eztv.py
@@ -1,8 +1,25 @@
 import requests
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def eztv_extract_tv_show_episodes(eztv_url, tmdb_show_id):
     # Extract the TV show episodes from the EZTV API
-    eztv_shows_request = requests.get(eztv_url + "/api/get-torrents?imdb_id=" + tmdb_show_id)
-    eztv_shows = eztv_shows_request.json()
-    return eztv_shows["torrents"]
+    eztv_shows = None
+    try:
+        eztv_shows_request = requests.get(eztv_url + "/api/get-torrents?imdb_id=" + tmdb_show_id)
+        eztv_shows = eztv_shows_request.json()
+    except requests.exceptions.ConnectionError as error:
+        logger.error("requests connection error: %s", error)
+    except requests.exceptions.Timeout as error:
+        logger.error("requests connection timeout: %s", error)
+    except requests.exceptions.HTTPError as error:
+        logger.error("requests HTTP error: %s", error)
+
+    if eztv_shows:
+        return eztv_shows["torrents"]
+    else:
+        logger.error(
+            "error while extracting the TV show episodes list from the EZTV REST API")
+        return eztv_shows
